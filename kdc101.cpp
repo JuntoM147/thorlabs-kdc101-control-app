49d485a7a8a6d679683487d220dc0406f73984a6
Junto Mabuchi <juntomabuchi@hotmail.com>
2026-07-30 17:49:33 +0800

    fix: find device fixed + added required DLLs list

--- kdc101.cpp
+++ kdc101.cpp
@@ -1,5 +1,6 @@
 #define DEVICE_ID 27
-#define SERIAL_NUMBER 26000001
+#define SERIAL_NUMBER 27000001
+#define SERIAL_NUMBER_STR "27000001"
 
 #include <stdlib.h>
 #include <conio.h>
@@ -22,35 +23,39 @@ int find_device(int serialNum) {
     // get KDC serial numbers
     char serialNumbers[250];
     TLI_GetDeviceListByTypeExt(serialNumbers, 250, DEVICE_ID);     // Get the device list which are dc servos
-    printf("%s \n", serialNumbers);
 
     // output list of matching devices
+    char *searchContext = NULL;
+    char *p = strtok_s(serialNumbers, ",", &searchContext);
+
+    int found = 0;
+
+    while (p != NULL)
     {
-        char *searchContext = NULL;
-        char *p = strtok_s(serialNumbers, ",", &searchContext);
+        TLI_DeviceInfo deviceInfo;
 
-        while (p != NULL)
-        {
-            TLI_DeviceInfo deviceInfo;
+        // get device info from device
+        TLI_GetDeviceInfo(p, &deviceInfo);
 
-            // get device info from device
-            TLI_GetDeviceInfo(p, &deviceInfo);
+        // get strings from device info structure
+        char curr_desc[65];
+        strncpy_s(curr_desc, deviceInfo.description, 64);
+        curr_desc[64] = '\0';
 
-            // get strings from device info structure
-            char desc[65];
-            strncpy_s(desc, deviceInfo.description, 64);
-            desc[64] = '\0';
-            char serialNo[9];
-            strncpy_s(serialNo, deviceInfo.serialNo, 8);
-            serialNo[8] = '\0';
+        char curr_serialNo[9];
+        strncpy_s(curr_serialNo, deviceInfo.serialNo, 8);
+        curr_serialNo[8] = '\0';
 
-            // output
-            printf("Found Device %s=%s : %s\r\n", p, serialNo, desc);
-            p = strtok_s(NULL, ",", &searchContext);
+        if (strncmp(curr_serialNo, SERIAL_NUMBER_STR, 8) == 0) {
+            found = 1;
         }
+
+        // output
+        printf("Found Device! Serial Number: %s : %s\r\n", curr_serialNo, curr_desc);
+        p = strtok_s(NULL, ",", &searchContext);
     }
 
-    return 0;
+    return found;
 }
 
 
@@ -59,10 +64,15 @@ int wmain(int argc, wchar_t* argv[]) // wmain is for windows, same with wchar_t 
     TLI_InitializeSimulations();
 
     // identify and access device
-    find_device(SERIAL_NUMBER);
+    if(find_device(SERIAL_NUMBER) == 0) {
+        printf("Failed to find device with serial number %d\r\n", SERIAL_NUMBER);
+        TLI_UninitializeSimulations();
+        return 1;
+    }
 
-    char testSerialNo[16] = "SERIAL_NUMBER";
-    sprintf_s(testSerialNo, "%d", SERIAL_NUMBER);
+    char testSerialNo[16] = SERIAL_NUMBER_STR;
+
+    printf("Found device with serial number %d\r\n", SERIAL_NUMBER);
 
     TLI_UninitializeSimulations();
 
