8deda9a88b945f91880c8251f090d713f48e1722
Junto Mabuchi <juntomabuchi@hotmail.com>
2026-09-16 18:01:46 +0800

    feat: added emergency stop section in UI

---
+++ src/frontend/emergency_stop/emergency_stop.h
@@ -0,0 +1,12 @@
+#ifndef FRONTEND_EMERGENCY_STOP_H
+#define FRONTEND_EMERGENCY_STOP_H
+
+class QWidget;
+
+namespace ui {
+
+QWidget* CreateEmergencyStopSection(QWidget* parent);
+
+}  // namespace ui
+
+#endif  // FRONTEND_EMERGENCY_STOP_H
--- src/frontend/main_window/main_window.cc
+++ src/frontend/main_window/main_window.cc
@@ -3,7 +3,9 @@
 #include <QHBoxLayout>
 #include <QStatusBar>
 #include <QWidget>
+#include <QVBoxLayout>
 
+#include "emergency_stop/emergency_stop.h"
 #include "laser_control/laser_control.h"
 #include "motor_control/motor_control.h"
 #include "scan/scan.h"
@@ -12,14 +14,30 @@ namespace ui {
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
   setWindowTitle(tr("Confo Quanta"));
-  resize(1100, 600);
+  resize(1400, 900);
+
   auto* central_widget = new QWidget(this);
+
   auto* layout = new QHBoxLayout(central_widget);
   layout->setContentsMargins(16, 16, 16, 16);
   layout->setSpacing(16);
-  layout->addWidget(CreateMotorControlSection(central_widget), 1);
+
+  // motor control
+  layout->addWidget(CreateMotorControlSection(central_widget), 2);
+  
+  // scan
   layout->addWidget(CreateScanSection(central_widget), 2);
-  layout->addWidget(CreateLaserControlSection(central_widget), 1);
+
+  // laser control and emergency stop
+  auto* right_column = new QWidget(central_widget);
+  auto* right_layout = new QVBoxLayout(right_column);
+  right_layout->setContentsMargins(0, 0, 0, 0);
+  right_layout->setSpacing(16);
+  right_layout->addWidget(CreateLaserControlSection(right_column));
+  right_layout->addWidget(CreateEmergencyStopSection(right_column));
+  right_layout->addStretch();
+  layout->addWidget(right_column, 1);
+
   setCentralWidget(central_widget);
   statusBar()->showMessage(tr("Hardware not connected."));
 }
